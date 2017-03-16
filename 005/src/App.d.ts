0b57fce6bd6919320d3879fbe9abc0eae09e1ede
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2017-03-16 12:45:33 +0330

    remove old 005

--- 005/src/App.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-export interface AppProps {
-    compiler: string;
-    framework: string;
-}
-declare class App extends React.Component<AppProps, undefined> {
-    render(): any;
-}
-export default App;
